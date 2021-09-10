eca6b42d1ed225c9c6e8b788b04d8cd47e5f3e23
prashish-bhandari <prashb.web@gmail.com>
2021-09-10 11:45:58 -0400

    completed task 7

--- index.js
+++ index.js
@@ -319,9 +319,17 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].paintings > 100) {
+      newArray.push(array[i].name);
+    }
+  }
+  return newArray;
 }
+
+console.log(lotsOfArt(artists));
 
 
 /* ***** END OF TASKS ***** */
